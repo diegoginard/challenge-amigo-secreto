77157632e89a9440b5be7db69b4198f2e67892b7
diegoginard <diegoginard@hotmail.com>
2025-02-05 00:05:17 -0300

    creacion de funciones

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+
+function agregarAmigo(){
+
+    let amigo = document.getElementById('amigo').value
+
+    amigos.push(amigo)
+
+    console.log(amigos)
+
+    document.getElementById('amigo').value = ''
+
+    document.getElementById('amigo').focus()
+
+    mostrarAmigos()
+}
+
+function mostrarAmigos(){
+
+    let amigosHTML = ''
+
+    for(let i = 0; i < amigos.length; i++){
+        amigosHTML += `<li>${amigos[i]}</li>`
+    }
+
+    document.getElementById('listaAmigos').innerHTML = amigosHTML
+}
+
+function eliminarAmigo(){
+    
+    amigos.pop()
+
+    mostrarAmigos()
+}
+
+function sortearAmigo(){
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)]
+
+    document.getElementById('resultado').innerHTML = amigoSorteado
+}
